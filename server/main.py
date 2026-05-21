d426a3133873b04f81adf586de0ca49ca84dc0cc
Kuldeep-singh24 <143269323+Kuldeep-singh24@users.noreply.github.com>
2026-05-21 17:14:17 +0530

    Fixed CORS issue

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from middlewares.exception_handlers import catch_exception_middleware
 from routes.upload_pdfs import router as upload_router
 from routes.ask_question import router as ask_router
@@ -12,19 +13,24 @@ app = FastAPI(
 # CORS Setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://medical-assistant-ai-eight.vercel.app",
+        "http://localhost:5173",
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Middleware exception handlers
+# middleware exception handlers
 app.middleware("http")(catch_exception_middleware)
 
-# Routers
-
-# 1. Upload PDF documents
+# routers
 app.include_router(upload_router)
-
-# 2. Asking query
 app.include_router(ask_router)
+
+
+@app.get("/")
+def home():
+    return {"message": "Medical Assistant API Running"}
