4cdd5db9e8553e2f268f4c5595be7cc6b3537f30
Kuldeep-singh24 <143269323+Kuldeep-singh24@users.noreply.github.com>
2026-05-21 16:57:41 +0530

    Fixed CORS middleware

--- server/main.py
+++ server/main.py
@@ -4,27 +4,27 @@ from middlewares.exception_handlers import catch_exception_middleware
 from routes.upload_pdfs import router as upload_router
 from routes.ask_question import router as ask_router
 
-
-
-app=FastAPI(title="Medical Assistant API",description="API for AI Medical Assistant Chatbot")
+app = FastAPI(
+    title="Medical Assistant API",
+    description="API for AI Medical Assistant Chatbot"
+)
 
 # CORS Setup
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
-
-
-# middleware exception handlers
+# Middleware exception handlers
 app.middleware("http")(catch_exception_middleware)
 
-# routers
+# Routers
 
-# 1. upload pdfs documents
+# 1. Upload PDF documents
 app.include_router(upload_router)
-# 2. asking query
+
+# 2. Asking query
 app.include_router(ask_router)
